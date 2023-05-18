7ef3f7d3d652bd642bff64d5eacb26ae73825bb6
Pratap_Singh_Sisodia <prataps135@gmail.com>
2023-05-18 21:40:30 +0530

    Angular setup

--- DB_file.sql
+++ DB_file.sql
@@ -2,15 +2,18 @@ create database movie;
 
 use movie;
 
+drop table users;
+drop table admin;
+
 create table users(id int auto_increment not null, userName varchar(100) not null, userPassword varchar(100)
  not null, userEmail varchar(100) not null,
- userContact int not null
+ userContact varchar(100) not null
  , primary key(id));
  
  select * from users;
  
  create table admin(id int auto_increment not null, adminName varchar(100) not null, adminPassword varchar(100)
- not null, adminEmail varchar(100) not null, adminContact int not null
+ not null, adminEmail varchar(100) not null, adminContact varchar(100) not null
  ,primary key(id));
  
  select * from admin;
